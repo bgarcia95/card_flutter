93362dc9e6dc095e4f80a8c1622eb6d846d3b678
bgarcia95 <bgarcia.95@outlook.com>
2019-06-15 18:24:35 -0600

    Learning how to use Column and Row with Flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,36 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            margin: EdgeInsets.only(top: 20.0, left: 15.0),
-            padding: EdgeInsets.all(20.0),
-            child: Text('Hello!'),
+          child: Row(
+            //crossAxisAlignment is for horizontal
+            //mainAxisAlignment is for vertical
+            // the following line is useful when you want the column to use the full screen from left-to-right or from top-to-bottom
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                //height will be used with Column and width will be used with Row
+                width: 20.0,
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              SizedBox(
+                //height will be used with Column and width will be used with Row
+                width: 20.0,
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
